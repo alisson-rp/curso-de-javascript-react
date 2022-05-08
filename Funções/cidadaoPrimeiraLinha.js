5df6c545dbe24e24ff7b2a1734b8135db023b5cd
Alisson Rabelo <alisson.rabelo.p@hotmail.com>
2022-05-08 11:40:50 -0300

    seção 05

---
+++ Funções/cidadaoPrimeiraLinha.js
@@ -0,0 +1,7 @@
+// Função em JS é First-Class Object (Citizens)
+// Higher-order function 
+
+// criar de forma literal
+function fun1() {
+    
+}
---
+++ fundamentos/erro.js
@@ -0,0 +1,17 @@
+// tratamento de erro 
+function tratarErroELancar(erro) {
+    throw new  Error('...')
+}
+
+function imprimirNomeGritado(obj) {
+    try{
+        console.log(obj.name.toUpperCase() + '!!!')
+    }  catch (e) {
+        tratarErroELancar(e)
+    }   finally {
+        console.log('final')
+    }    
+}
+
+const obj = { nome: 'Roberto'}
+imprimirNomeGritado(obj)
